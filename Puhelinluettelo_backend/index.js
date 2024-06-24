fc6cbc6754854fa8abfd62a3ca574ae6cb4ab96b
Veikka Hakulinen <2205790@edu.karelia.fi>
2024-06-24 14:27:32 +0300

    teht 3.22

--- Puhelinluettelo_backend/index.js
+++ Puhelinluettelo_backend/index.js
@@ -1,107 +1,107 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const Person = require("./models/personMongo");
-app.use(express.json());
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Person = require('./models/personMongo')
+app.use(express.json())
 
-const cors = require("cors");
-app.use(cors());
+const cors = require('cors')
+app.use(cors())
 
 //sov render
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 //all
-app.get("/api/persons", (request, response, next) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((persons) => {
-      response.json(persons);
+      response.json(persons)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 //info
-app.get("/info", (request, response) => {
-  let text = `Phonebook has info for ${persons.length}`;
-  let infoText = "<p>" + text + "</p>" + "<p>" + new Date() + "</p>";
+/*app.get('/info', (request, response) => {
+  let text = `Phonebook has info for ${persons.length}`
+  let infoText = '<p>' + text + '</p>' + '<p>' + new Date() + '</p>'
 
-  console.log(infoText);
-  console.log(persons);
-  console.log(new Date());
-  response.send(infoText);
-});
+  console.log(infoText)
+  console.log(persons)
+  console.log(new Date())
+  response.send(infoText)
+})*/
 
 //person by id
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((returnedPerson) => {
       if (returnedPerson) {
-        response.json(returnedPerson);
+        response.json(returnedPerson)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 //delete person
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 //add new person
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
-  console.log("Post body", body);
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  console.log('Post body', body)
 
   if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ error: "content missing", body });
+    return response.status(400).json({ error: 'content missing', body })
   }
 
   const newPerson = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
-  console.log(newPerson);
+  console.log(newPerson)
 
   newPerson
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
-app.use(errorHandler);
+app.use(errorHandler)
 
 //app use port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
 
 /*let persons = [
   {
--- Puhelinluettelo_backend/models/personMongo.js
+++ Puhelinluettelo_backend/models/personMongo.js
@@ -1,34 +1,34 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGO_URI;
+const url = process.env.MONGO_URI
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
 //yhdistys
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
   .then((result) => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 //muodon määrittely
 const personSchema = new mongoose.Schema({
   name: { type: String, minlength: 3, required: true },
   number: String,
-});
+})
 
 //id korjaus string muotoon
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
